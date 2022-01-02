09d72672b6e7cdc061ab7230a75d9a0d637940ba
Akshat <akshatjaiswal729@gmail.com>
2022-01-02 18:27:06 +0530

    1.1

---
+++ Goldman Sachs/1.py
@@ -0,0 +1,45 @@
+#User function Template for python3
+
+class Solution:
+    def Anagrams(self, words, n):
+        '''
+        words: list of word
+        n:      no of words
+        return : list of group of anagram {list will be sorted in driver code (not word in grp)}
+        '''
+        
+        #code here
+        
+        def returnSorted(s):
+            l = list(s)
+            l.sort()
+            return str(l)
+        ans = {}
+        for i in words:
+            val = returnSorted(i)
+            if val in ans:
+                ans[val].append(i)
+            else:
+                ans[val] = [i]
+        return list(ans.values())
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+#contributed by RavinderSinghPB
+if __name__ =='__main__':
+    t= int(input())
+    for tcs in range(t):
+        n= int(input())
+        words=input().split()
+        
+        ob = Solution()
+        ans = ob.Anagrams(words,n)
+        
+        for grp in sorted(ans):
+            for word in grp:
+                print(word,end=' ')
+            print()
+
+# } Driver Code Ends
