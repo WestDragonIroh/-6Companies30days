b005d449fce1aac7aba15930a040e1b0a1806192
Akshat <akshatjaiswal729@gmail.com>
2022-01-13 17:44:42 +0530

    1.7

---
+++ Goldman Sachs/7.py
@@ -0,0 +1,11 @@
+def lastPosition(n, m, k):
+    
+    if (m <= n - k + 1):
+       return m + k - 1
+ 
+    m = m - (n - k + 1)
+
+    if(m % n == 0):
+        return n
+    else:
+        return m % n
