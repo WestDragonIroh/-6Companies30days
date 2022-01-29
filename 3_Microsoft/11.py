afb78464ba62ffcf705aefe67427cebea56750c7
Akshat <akshatjaiswal729@gmail.com>
2022-01-29 20:44:47 +0530

    3.11

---
+++ 3_Microsoft/11.py
@@ -0,0 +1,46 @@
+#User function Template for python3
+
+
+
+#Function to generate binary numbers from 1 to N using a queue.
+def generate(N):
+    # code here
+    res = [1]
+    for num in range(2, N+1):
+        binary = 0
+        ctr = 0
+        while num > 0:
+            binary += ((num % 2)*(10**ctr))
+            num = num//2
+            ctr += 1
+        res.append(binary)
+    return res
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+import atexit
+import io
+import sys
+
+#Contributed by :  Nikhil Kumar Singh(nickzuck_007)
+
+_INPUT_LINES = sys.stdin.read().splitlines()
+input = iter(_INPUT_LINES).__next__
+_OUTPUT_BUFFER = io.StringIO()
+sys.stdout = _OUTPUT_BUFFER
+
+@atexit.register
+
+def write():
+    sys.__stdout__.write(_OUTPUT_BUFFER.getvalue())
+
+if __name__ == '__main__':
+    test_cases = int(input())
+    for cases in range(test_cases) :
+        n = int(input())
+        res = generate(n)
+        for i in range (len (res)):
+            print (res[i], end=" ")
+        print()
+# } Driver Code Ends
